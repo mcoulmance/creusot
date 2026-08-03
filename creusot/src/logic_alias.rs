4658949f6d66cd68d0e7ff6aa671ebb565978950
Maël Coulmance <mcoulmance@gmail.com>
2026-08-03 14:44:14 +0200

    fix subst_call, move it to pearlite::from_thir

--- creusot/src/logic_alias.rs
+++ creusot/src/logic_alias.rs
@@ -6,7 +6,7 @@ use crate::{
 use itertools::{EitherOrBoth, Itertools};
 use rustc_hir::{def::DefKind, def_id::DefId};
 use rustc_middle::ty::{
-    GenericArg, GenericArgsRef, TyCtxt,
+    EarlyBinder, GenericArg, GenericArgsRef, TyCtxt, TypingEnv,
     fast_reject::{TreatParams, simplify_type},
 };
 use rustc_span::Span;
@@ -26,7 +26,9 @@ pub(crate) fn get_logic_id(ctx: &TranslationCtx, def_id: DefId) -> DefId {
 
 pub(crate) fn subst_call<'tcx, Args>(
     ctx: &TranslationCtx<'tcx>,
+    typing_env: TypingEnv<'tcx>,
     prog_id: DefId,
+    prog_subst: GenericArgsRef<'tcx>,
     prog_args: Args,
 ) -> Option<(DefId, Box<[Term<'tcx>]>, GenericArgsRef<'tcx>)>
 where
@@ -35,22 +37,33 @@ where
     if let Some((_, alias_id)) = ctx.logic_alias(prog_id) {
         let ensures_body =
             ctx.raw_term(alias_id).expect("no ensures clause associated with this alias");
+
         match &ensures_body.1.kind {
             TermKind::Binary { rhs, .. } => match &rhs.kind {
                 TermKind::Call { id, args, subst: call_subst } => {
-                    let mut subst = MapSubstitution::new();
+                    let mut args_subst = MapSubstitution::new();
 
                     let prog_params = &ensures_body.0;
                     for (param, term) in
                         itertools::zip_eq(&prog_params[..prog_params.len() - 1], prog_args)
                     {
-                        subst.insert(param.0, term.kind);
+                        args_subst.insert(param.0, term.kind);
                     }
 
                     let helper_subst = |mut term: Term<'tcx>| {
-                        term.subst(&subst);
+                        term = ctx.normalize_erasing_regions(
+                            typing_env,
+                            EarlyBinder::bind(/* self.tcx */ term).instantiate(ctx.tcx, prog_subst),
+                        );
+                        term.subst(&args_subst);
                         term
                     };
+
+                    let call_subst = ctx.normalize_erasing_regions(
+                        typing_env,
+                        EarlyBinder::bind(/* self.tcx */ *call_subst)
+                            .instantiate(ctx.tcx, prog_subst),
+                    );
 
                     let res_args = args.iter().map(|term| helper_subst(term.clone())).collect();
                     Some((*id, res_args, call_subst))
